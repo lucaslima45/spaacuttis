3d06344736bac1508ef7b449312fd58b961a7dcb
Lucas Henrique Lima <lucashlima45@gmail.com>
2022-05-23 23:24:51 -0300

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import style from './Header.module.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header flexItem">
+                <h2>Calculadora de Jornada de Trabalho</h2>
+                <h6>O colaborador não pode ter períodos com virada de dia.</h6>
+                
+                
+                
+                <br /><br />
+            </div>
+        );
+    }
+}
+
+export default Header;
